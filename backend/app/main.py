a446e1e0407f691088711ad36e08d9d609eca694
Wbeimar <wbeimar224@gmail.com>
2026-03-17 13:49:25 -0500

    v4

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,22 +1,28 @@
 from fastapi import FastAPI
-import logging
-
 from app.core.database import engine, Base
 from app.core.exception_handlers import global_exception_handler
-from app.models.usuario import Usuario
-from app.models.rol import Rol
-
+#from app.models.usuario import Usuario
+#from app.models.rol import Rol
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.auth_router import router as auth_router
 from app.api.proyectos_router import router as proyectos_router
 from app.api.usuarios_router import router as usuarios_router
+import logging
 
-# Configurar logger
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI(
     title="Plataforma de Seguimiento",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Base.metadata.create_all(bind=engine)
@@ -32,6 +38,14 @@ logger.info("API iniciada correctamente")
 def root():
     return {"message": "API funcionando correctamente"}
 
-@app.get("/error-test")
-def error_test():
-    raise Exception("Error de prueba")
+#@app.get("/error-test")
+#def error_test():
+#    raise Exception("Error de prueba")
+
+@app.get("/health")
+def health_check():
+    return {
+        "status": "ok",
+        "service": "backend",
+        "version": "1.0"
+    }
