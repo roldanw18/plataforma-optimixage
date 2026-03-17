d9acf92d3eeaa4e02debce761bd2dadd4d960241
Wbeimar <wbeimar224@gmail.com>
2026-03-16 22:22:23 -0500

    bdv2

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-
 DATABASE_URL = "postgresql://postgres:1@localhost:5432/optimixagedb"
+
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
@@ -13,5 +13,14 @@ SessionLocal = sessionmaker(
 
 Base = declarative_base()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# importar modelos para que SQLAlchemy los registre
 from app.models.usuario import Usuario
 from app.models.rol import Rol
--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,16 +1,19 @@
+import uuid
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy.pool import StaticPool
 from app.main import app
 from app.core.database import Base, get_db
+
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool
 )
 
 TestingSessionLocal = sessionmaker(
@@ -48,3 +51,58 @@ def client(db):
     yield TestClient(app)
 
     app.dependency_overrides.clear()
+
+
+@pytest.fixture
+def auth_token(client):
+
+    email = f"test_{uuid.uuid4()}@test.com"
+    password = "123456"
+
+    client.post(
+        "/auth/register",
+        json={
+            "nombre": "Test User",
+            "email": email,
+            "password": password
+        }
+    )
+
+    response = client.post(
+        "/auth/login",
+        data={
+            "username": email,
+            "password": password
+        }
+    )
+
+    assert response.status_code == 200
+
+    return response.json()["access_token"]
+
+
+# 👇 Fixture para usuario admin
+@pytest.fixture
+def admin_token(client):
+
+    email = f"admin_{uuid.uuid4()}@test.com"
+    password = "123456"
+
+    client.post(
+    "/auth/register",
+    json={
+        "nombre": "Admin User",
+        "email": email,
+        "password": password
+    }
+)
+
+    response = client.post(
+        "/auth/login",
+        data={
+            "username": email,
+            "password": password
+        }
+    )
+
+    return response.json()["access_token"]
