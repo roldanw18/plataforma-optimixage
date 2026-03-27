9a6dd04454e50844a344115c3f31c2bf7b82ac9d
Wbeimar <wbeimar224@gmail.com>
2026-03-27 14:11:14 -0500

    test debugs

--- backend/app/schemas/usuario_schema.py
+++ backend/app/schemas/usuario_schema.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel, EmailStr
 from uuid import UUID
 
@@ -6,6 +8,7 @@ class UsuarioCreate(BaseModel):
     nombre: str
     email: EmailStr
     password: str
+    rol: Optional[str] = "cliente"
 
 
 class UsuarioLogin(BaseModel):
