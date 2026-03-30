b7e9bb3786a2a5bfb6267f17739196062fd81e74
Wbeimar <wbeimar224@gmail.com>
2026-03-30 14:16:51 -0500

    Prueba 5

--- backend/app/api/proyectos_router.py
+++ backend/app/api/proyectos_router.py
@@ -5,23 +5,25 @@ from app.core.database import get_db
 from app.core.dependencies import get_current_user
 from app.schemas.proyecto_schema import ProyectoCreate
 from app.models.proyecto import Proyecto
-
+from app.models.usuario import Usuario
+from app.schemas.proyecto_schema import ProyectoCreate, ProyectoResponse
 router = APIRouter(tags=["Proyectos"])
 
-
-@router.post("/")
+@router.post("/", response_model=ProyectoResponse)
 def crear_proyecto(
     data: ProyectoCreate,
     db: Session = Depends(get_db),
-    current_user=Depends(get_current_user)
+    current_user: Usuario = Depends(get_current_user)
 ):
-    # TEMPORAL (hasta implementar roles bien)
-    # if current_user.rol.nombre != "admin":
-    #     raise HTTPException(status_code=403, detail="No autorizado")
+    
+    if current_user.rol.nombre != "admin":
+        raise HTTPException(
+            status_code=403,
+            detail="No autorizado"
+        )
 
     proyecto = Proyecto(
-        nombre=data.nombre,
-        descripcion=data.descripcion
+        nombre=data.nombre
     )
 
     db.add(proyecto)
@@ -29,7 +31,6 @@ def crear_proyecto(
     db.refresh(proyecto)
 
     return proyecto
-
 
 @router.get("/mis-proyectos")
 def listar_proyectos(
