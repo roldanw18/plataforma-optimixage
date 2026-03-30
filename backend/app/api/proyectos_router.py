5e823338fb0963670e30216d8f79428bd2febeec
Wbeimar <wbeimar224@gmail.com>
2026-03-30 13:03:37 -0500

    Prueba 2  30/03/2026

--- backend/app/api/proyectos_router.py
+++ backend/app/api/proyectos_router.py
@@ -1,19 +1,12 @@
 from fastapi import HTTPException, APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.core.database import SessionLocal
+
+from app.core.database import get_db
 from app.core.dependencies import get_current_user
 from app.schemas.proyecto_schema import ProyectoCreate
 from app.models.proyecto import Proyecto
 
 router = APIRouter(tags=["Proyectos"])
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 
 @router.post("/")
@@ -22,8 +15,9 @@ def crear_proyecto(
     db: Session = Depends(get_db),
     current_user=Depends(get_current_user)
 ):
-    if current_user.rol.nombre != "admin":
-        raise HTTPException(status_code=403, detail="No autorizado")
+    # TEMPORAL (hasta implementar roles bien)
+    # if current_user.rol.nombre != "admin":
+    #     raise HTTPException(status_code=403, detail="No autorizado")
 
     proyecto = Proyecto(
         nombre=data.nombre,
